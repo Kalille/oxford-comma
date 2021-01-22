9f74d43c16ab25aa182adee7723839e647e867c0
kalille Overton <>
2021-01-22 16:10:12 -0500

    commit tree 0c9bf1515f1e836bb8d7e3004c03dc5d8fdd9c38

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require "pry"
 def oxford_comma(array)
-
+  if array.length < 2
+     array.join 
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    last_index=array.length-1
+    array[last_index]= "and " + array[last_index]
+    array.join(", ")
+  end
+  
 end
+
+
+
